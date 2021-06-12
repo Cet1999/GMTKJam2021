f7c79f776158402d7763f559519070843948c4a4
2020wmarvil <2020wmarvil@gmail.com>
2021-06-11 22:31:38 -0400

    Movement

--- GMTK/Assets/Scripts/Player/BigPlayer_Movement.cs
+++ GMTK/Assets/Scripts/Player/BigPlayer_Movement.cs
@@ -1,18 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class BigPlayer_Movement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class BigPlayer_Movement : MonoBehaviour {
+    Rigidbody rb;
+
+    Vector3 rawInputMovement;
+    bool jumpPressed;
+    bool isGrounded;
+
+    [SerializeField] float maxVelocityWhileGrounded = 5f;
+    [SerializeField] float maxVelocityWhileFalling = 3f;
+    [SerializeField] float moveForce = 10000f;
+    [SerializeField] float jumpForce = 50000f;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void Update() { }
+
+    private void FixedUpdate() {
+        // set isGrounded
+        float distToGround = GetComponent<CapsuleCollider>().bounds.extents.y;
+        isGrounded = Physics.Raycast(rb.position, -Vector3.up, distToGround + 0.1f);
+
+        // calculate force applied by input
+        Vector3 moveDir = rawInputMovement.normalized;
+        Vector3 force = moveDir * moveForce;
+
+        if (!isGrounded) {
+            force *= 0.5f;
+        }
         
+        rb.AddForce(force);
+
+        // clamp velocity
+        Vector3 vel = rb.velocity;
+	    vel.y = 0;
+
+        if (isGrounded) {
+	        vel = Vector3.ClampMagnitude(vel, maxVelocityWhileGrounded);
+        } else {
+	        vel = Vector3.ClampMagnitude(vel, maxVelocityWhileFalling);
+        }
+
+	    vel.y = rb.velocity.y;
+	    rb.velocity = vel;
+
+        // attempt to jump
+        if (jumpPressed && isGrounded) {
+            rb.AddForce(Vector3.up * jumpForce);
+        }
     }
+
+    public void onMovement(InputAction.CallbackContext value) { 
+        Vector2 inputMovement = value.ReadValue<Vector2>();
+        rawInputMovement = new Vector3(inputMovement.x, 0f, inputMovement.y);
+    } 
+
+    public void onJump(InputAction.CallbackContext value) {
+        if (value.started) jumpPressed = true;
+        if (value.canceled) jumpPressed = false;
+    } 
 }
