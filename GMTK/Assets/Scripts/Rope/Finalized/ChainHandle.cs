266685570c9a4078b65cf0d36f21456eb319bdb6
noahm1216 <noahm1216@gmail.com>
2021-06-12 09:04:47 -0700

    CHAIN WORKING
    - chain working now! as expected
    - going to try another chain that is longer and smaller

--- GMTK/Assets/Scripts/Rope/Finalized/ChainHandle.cs
+++ GMTK/Assets/Scripts/Rope/Finalized/ChainHandle.cs
@@ -12,6 +12,8 @@ public class ChainHandle : MonoBehaviour
 
     [Range(-10.0f, 10.0f)]
     public float distanceOffset = 1.0f;
+    [Range(-10.0f, 10.0f)]
+    public float distanceOffsetFinal = 1.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +25,7 @@ public class ChainHandle : MonoBehaviour
         Gizmos.color = Color.yellow;
         if (firstChain != null)
         {
-            Gizmos.DrawWireSphere(firstChain.position, distance);
+            Gizmos.DrawWireSphere(firstChain.position, distance * distanceOffsetFinal);
         }
     }
 
@@ -32,7 +34,7 @@ public class ChainHandle : MonoBehaviour
         // towards "player"
         Vector3 dir = player.transform.position - firstChain.transform.position;
         // clamp length
-        dir = Vector3.ClampMagnitude(dir, distance);
+        dir = Vector3.ClampMagnitude(dir, distance * distanceOffsetFinal);
         // add clamped length
         transform.position = firstChain.transform.position + dir;
         // look towards movement
