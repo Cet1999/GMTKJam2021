9eb52341854819dd70926266c687d16e9394cde8
2020wmarvil <2020wmarvil@gmail.com>
2021-06-13 09:59:55 -0400

    Add UI to the level

--- GMTK/Assets/Scripts/InGameUI.cs
+++ GMTK/Assets/Scripts/InGameUI.cs
@@ -9,12 +9,16 @@ public class InGameUI : MonoBehaviour {
 		bigPlayerInput.SwitchCurrentActionMap("WASD");
 		smallPlayerInput.SwitchCurrentActionMap("ArrowKeys");
 
+		Time.timeScale = 1f;
+
 		gameObject.SetActive(false);
 	}
 
 	public void PauseGame() { 
 		bigPlayerInput.SwitchCurrentActionMap("InMenu");
 		smallPlayerInput.SwitchCurrentActionMap("InMenu");
+
+		Time.timeScale = 0f;
 	}
 
 	public void ExitGame() => Application.Quit();
